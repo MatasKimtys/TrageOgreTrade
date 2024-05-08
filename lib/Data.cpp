e870c12486d7a0787d83a85c5a8ae9485cbb3dac
Matas Kimtys <mataskimtys@gmail.com>
2024-05-08 21:11:18 +0100

    Remove nlohmann leftovers

--- lib/Data.cpp
+++ lib/Data.cpp
@@ -10,18 +10,3 @@ DataHolder::~DataHolder()
     
 }
 
-// nlohmann::json DataHolder::getMarkets()
-// {
-//     return nlohmann::json();
-// }
-
-std::string DataHolder::getTickers()
-{
-    return std::string();
-}
-
-std::string DataHolder::getTradeHistory()
-{
-    return std::string();
-}
-
